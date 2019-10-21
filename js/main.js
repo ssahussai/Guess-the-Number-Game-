74b255002e9e926ad9a8a175ea2fd06b2790df5f
Salma Khan <hussainsalma37@gmail.com>
2019-10-21 14:57:02 -0500

    added getGuess method

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ const game = {
     biggestNum: 100,
     smallestNum: 1,
     secretNum: null,
+    prevGuesses: [],
+    getGuess: function() {
+     do {
+       var guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`));     
+     } 
+     while(guess < this.smallestNum || guess > this.biggestNum || isNaN(guess));  
+    },
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
     }
   };
 
-  game.prevGuesses = [];
+  game.getGuess();
