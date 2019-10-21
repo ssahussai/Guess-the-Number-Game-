f34ef40567d5634a5d90ee286eeac24f1f0f893a
Salma Khan <hussainsalma37@gmail.com>
2019-10-21 16:11:00 -0500

    created and invoked render method

--- js/main.js
+++ js/main.js
@@ -8,14 +8,28 @@ const game = {
      do {
        var guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`));     
      } 
-     while(guess < this.smallestNum || guess > this.biggestNum || isNaN(guess));  
+     while(guess < this.smallestNum || guess > this.biggestNum || isNaN(guess)); 
+     this.prevGuesses.push(guess);
+     this.render(guess);
     },
     play: function() {
       this.secretNum = Math.floor(Math.random() * 
         (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
         this.getGuess();
-        prevGuesses.push(guess);
+    },
+    render: function(guess) {
+      if (guess === this.secretNum) {
+        alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses`);
+      } else if (guess < this.secretNum) {
+        alert(`Your guess is too low. Previous guesses: ${this.prevGuesses.join(' ,')}`);
+      } else if (guess > this.secretNum) {
+        alert(`Your guess is too high. Previous guesses: ${this.prevGuesses.join(' ,')}`);
+      }
     }
-  };
+    };
+    
+   game.play();
+    
+
 
   
